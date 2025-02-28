d28b3aad251992a4586d93dde59a3f1d56ee7202
Guis-gk <karina.27.guise@gmail.com>
2025-02-28 14:04:07 -0500

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigo= [];
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if(!nombreAmigo){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value= "";
+    inputAmigo.focus();
+    renderizarAmigos();
+}
